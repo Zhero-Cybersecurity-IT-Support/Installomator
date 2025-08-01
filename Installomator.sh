a81d41a845b4395309e6e4b5cdc97e627310a1cf
lo-zhero <124682522+Louis-Zhero@users.noreply.github.com>
2025-08-01 13:00:38 +0100

    Update Installomator.sh
    Updated notion to newest

--- Installomator.sh
+++ Installomator.sh
@@ -4832,16 +4832,10 @@ nordlayer)
     expectedTeamID="W5W395V82Y"
     ;;
 notion)
-    # credit: Søren Theilgaard (@theilgaard)
     name="Notion"
     type="dmg"
-    if [[ $(arch) == "arm64" ]]; then
-        downloadURL="https://www.notion.so/desktop/apple-silicon/download"
-        appNewVersion=$( curl -fsIL "https://www.notion.so/desktop/apple-silicon/download" | grep -i "^location" | awk '{print $2}' | sed -e 's/.*Notion\-\(.*\)\-arm64.dmg.*/\1/' )
-    elif [[ $(arch) == "i386" ]]; then
-        downloadURL="https://www.notion.so/desktop/mac/download"
-        appNewVersion=$( curl -fsIL "https://www.notion.so/desktop/mac/download" | grep -i "^location" | awk '{print $2}' | sed -e 's/.*Notion\-\(.*\).dmg.*/\1/' )
-    fi
+    downloadURL="https://www.notion.so/desktop/mac/download"
+    appNewVersion=$(curl -fsIL "https://www.notion.so/desktop/mac/download" | grep -i "^location" | awk '{print $2}' | sed -e 's/.*Notion-\(.*\).dmg.*/\1/' | cut -d '-' -f 1)
     expectedTeamID="LBQJ96FQ8D"
     ;;
 nudge)
